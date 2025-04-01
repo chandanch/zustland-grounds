00300a48c87adde6cac6ccb7517baac51ddff3aa
chandanch <chandan.cec107@gmail.com>
2025-04-01 22:29:57 +0530

    ✨ optimize task filtering in Column component using shallow comparison

--- src/components/column/Column.jsx
+++ src/components/column/Column.jsx
@@ -1,11 +1,14 @@
 import { useStore } from "zustand";
 import Task from "../task/Task";
 import "./Column.css";
+import { shallow } from "zustand/shallow";
 
 function Column({ state }) {
-  const tasks = useStore((store) => store.tasks);
-  const filteredTasks = tasks.filter((task) => task.state === state);
-  console.log(filteredTasks);
+  const tasks = useStore(
+    (store) => store.tasks.filter((task) => task.state === state),
+    shallow
+  );
+  console.log(tasks);
   return (
     <div className="column">
       <p> {state} </p>
