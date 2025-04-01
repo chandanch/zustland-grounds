1d01fcc96804c031decf7ec0b811d438de9531ae
chandanch <chandan.cec107@gmail.com>
2025-04-01 22:27:39 +0530

    ✨ integrate zustand store to filter tasks by state in Column component

--- src/components/column/Column.jsx
+++ src/components/column/Column.jsx
@@ -1,7 +1,11 @@
+import { useStore } from "zustand";
 import Task from "../task/Task";
 import "./Column.css";
 
 function Column({ state }) {
+  const tasks = useStore((store) => store.tasks);
+  const filteredTasks = tasks.filter((task) => task.state === state);
+  console.log(filteredTasks);
   return (
     <div className="column">
       <p> {state} </p>
