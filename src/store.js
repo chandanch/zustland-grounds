e7cffa2e179c681b182846fe80afe508090904ae
chandanch <chandan.cec107@gmail.com>
2025-04-02 20:35:06 +0530

    ✨ remove unused set argument

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import { create } from "zustand";
 
-const store = (set) => ({
+const store = () => ({
   tasks: [
     { title: "Task 1", state: "PLANNED" },
     { title: "Task 2", state: "ONGOING" },
