0c0dd6f785ac8db980039701c775ad9d3305a9b8
chandanch <chandan.cec107@gmail.com>
2025-04-03 14:57:17 +0530

    ✨ add delete functionality to Task component with delete icon

--- src/components/task/Task.jsx
+++ src/components/task/Task.jsx
@@ -1,16 +1,21 @@
 import classNames from "classnames";
 import "./Task.css";
 import { useStore } from "../../store";
+import deleteIcon from "../../assets/delete.svg";
 
 function Task({ title }) {
   const tasks = useStore((store) => store.tasks);
   const task = tasks.find((task) => task.title === title);
 
+  const deleteTask = useStore((store) => store.deleteTask);
+
   return (
     <div className="task">
       <div>{task.title}</div>
       <div className="bottom-wrapper">
-        <div></div>
+        <div>
+          <img src={deleteIcon} onClick={() => deleteTask(task.title)} />
+        </div>
         <div className={classNames("status", task.state)}> {task.state} </div>
       </div>
     </div>
